dbea93eeaef3eb5213ee1d48ef007b20be2d9134
AEpolyakov <ae.polyakov@mail.ru>
2021-09-14 12:56:56 +0400

    path fix

--- log/client_log_config.py
+++ log/client_log_config.py
@@ -1,8 +1,9 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
 
-handler = RotatingFileHandler(filename='./log/client.log', maxBytes=2000, backupCount=10)
+handler = RotatingFileHandler(filename=os.path.join('.', 'log', 'client.log'), maxBytes=2000, backupCount=10)
 formater = logging.Formatter("%(asctime)s %(levelname)s %(pathname)s %(message)s")
 handler.setFormatter(formater)
 
