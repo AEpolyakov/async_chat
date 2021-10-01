abaa9999a57e991f3f4f3fbce892e56fabd20fbe
AEpolyakov <ae.polyakov@mail.ru>
2021-10-01 09:37:37 +0400

    task1-3 fix

--- lesson9.py
+++ lesson9.py
@@ -1,59 +1,68 @@
 # задание 1
 import subprocess
 import ipaddress
+import socket
 import re
 import tabulate
-import os
 
 
-def host_ping(ip_list: list):
-    for ip in ip_list:
-        process = subprocess.Popen(['ping', '-c', '1', ip], stdout=subprocess.PIPE)
+def host_ping(host_list: list, printable=True):
+    result_dict = {"reachable": [],
+                   "unreachable": []}
+
+    for host in host_list:
+        try:
+            ip_address = ipaddress.ip_address(host)
+        except:
+            try:
+                ip_address = ipaddress.ip_address(socket.gethostbyname(host))
+            except:
+                ip_address = ''
+
+        process = subprocess.Popen(['ping', '-c', '1', str(ip_address)], stdout=subprocess.PIPE)
         string = process.stdout.read().decode('utf-8')
         try:
             result = re.findall(r'time=.*', string)[0]
-            print(f'Узел "{ip}" доступен')
         except Exception:
-            print(f'Узел "{ip}" НЕдоступен')
+            result_dict["unreachable"].append(host)
+            if printable:
+                print(f'Узел "{host}" НЕдоступен')
+        else:
+            result_dict["reachable"].append(host)
+            if printable:
+                print(f'Узел "{host}" доступен')
+
+    return result_dict
 
 
-ip_list = ['127.0.0.1', '127.0.0.2', '192.168.142.4', '192.168.142.200']
-# host_ping(ip_list)
+host_list = ['127.0.0.1', '127.0.0.2', 'google.com', '192.168.142.200']
+host_ping(host_list)
 
 
 # задание 2
 
 
-def host_range_ping(ip: str, ip_range: int):
-    ip_address = ipaddress.ip_address(ip)
-    ip_list = [str(ip_address + i) for i in range(ip_range)]
-    host_ping(ip_list)
+def host_range_ping(ip_range: str, printable=True):
+    try:
+        ip_network = ipaddress.ip_network(ip_range)
+    except Exception as ex:
+        print(f'bad ip range {ex}')
+    else:
+        ip_list = [ip_address for ip_address in ip_network]
+        return host_ping(host_list=ip_list, printable=printable)
 
 
-# host_range_ping('127.0.0.1', 5)
+host_range_ping('127.0.0.0/27')
 
 # задание 3
 
-def host_range_ping_tab(ip_start: str, ip_range: int):
 
-    ip_address = ipaddress.ip_address(ip_start)
-
-    result_dict = {"reachable": [],
-                   "unreachable": []}
-
-    for i in range(ip_range):
-        ip = ip_address + i
-        process = subprocess.Popen(['ping', '-c', '1', str(ip)], stdout=subprocess.PIPE)
-        string = process.stdout.read().decode('utf-8')
-        try:
-            result = re.findall(r'time=' + r'.*', string)[0]
-            result_dict["reachable"].append(ip)
-        except Exception:
-            result_dict["unreachable"].append(ip)
+def host_range_ping_tab(ip_range: str):
+    result_dict = host_range_ping(ip_range, printable=False)
     print(tabulate.tabulate(result_dict, headers="keys"))
 
 
-# host_range_ping_tab('127.0.0.253', 5)
+host_range_ping_tab('127.0.0.0/27')
 
 
 # задание 4
@@ -70,5 +79,5 @@ def start_clients(client_list: list):
 
 
 port = 7777
-client_list = [f'client.py -l u4 -p {port}', f'client.py -l u5 -t u1 -p {port}']
+client_list = [f'client.py -l user4 -p {port}', f'client.py -l user5 -t user1 -p {port}']
 start_clients(client_list)
