cbe3b5d0f192e3248f2b94aab1aaf21f12651460
AEpolyakov <ae.polyakov@mail.ru>
2021-10-05 09:26:33 +0400

    attr check

--- lesson10.py
+++ lesson10.py
@@ -1,19 +1,31 @@
 from client import Client
 import dis
+import re
 
 
 class ClientClassVerifier:
     def __init__(self, client_class):
         self.examined_class = client_class
+        with open('client_dis.txt', 'w') as f:
+            dis.dis(Client, file=f)
 
-    def check(self):
-        print(dis.dis(self.examined_class))
-        return
+        forbidden = ['accept', 'listen']
+        must_be = ['socket', 'AF_INET', 'SOCK_STREAM']
+        with open('client_dis.txt', 'r') as f:
+            disasm = f.read()
+            for string in forbidden:
+                result = re.findall(rf"LOAD_METHOD.*\({string}\)", disasm)
+                if result:
+                    raise AttributeError(f"class {self.examined_class} should not have {string}: {result}")
+
+            for string in must_be:
+                result = re.findall(rf"LOAD_GLOBAL.*\({string}\)", disasm)
+                if not result:
+                    raise AttributeError(f"class {self.examined_class} should have {string}")
+
+
+        print(f'class {self.examined_class} is valid')
 
 
 if __name__ == "__main__":
-    client_class_verifier = ClientClassVerifier(Client)
-    # client = Client(('address', 7777), 'login', 'to', 'mode')
-    # client_verifier = ClientVerifier(client)
-
-    print(client_class_verifier.check())
+    verifier = ClientClassVerifier(Client)
